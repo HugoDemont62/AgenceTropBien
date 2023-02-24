c54d5bff87d5dfad0c68110642ef108d814712f0
Legrand <kevinlegrand0113@gmail.com>
2023-02-24 18:21:37 +0100

    Kevin 24 02 2023

--- js/script.js
+++ js/script.js
@@ -1,27 +1,59 @@
 
+let repeat = 0
+let yoyo = false
+
+var lastScrollTop = 0;
+
+window.addEventListener('scroll', () => {
+        let st = this.scrollY;
+        if (st > lastScrollTop){
+            console.log("scroll vers le bas");
+            yoyo = false
+            repeat = 1
+        } else {
+            yoyo = true
+            repeat = 0
+            console.log("scroll vers le haut");
+        }
+        lastScrollTop = st;
+});
+
 gsap.registerPlugin(ScrollTrigger)
 
 gsap.from(".article__gsap__left", {
-        x: -100,
-        duration: 1,
-        opacity: 0,
-        scrollTrigger: {
-            trigger: ".article",
-            start: "top center",
-            end: "bottom center",
-        }
+    x: -100,
+    duration: 1,
+    opacity: 0,
+    yoyo : repeat,
+    repeat: yoyo,
+    scrollTrigger: {
+        trigger: ".article",
+        start: "top center",
+        end: "bottom center",
+    }
 })
 
 gsap.from(".article__gsap__right", {
-        x: 100,
-        duration: 1,
-        opacity: 0,
-        scrollTrigger: {
-            trigger: ".article",
-            start: "top center",
-            end: "bottom center",
-        }
+    x: 100,
+    duration: 1,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: ".article",
+        start: "top center",
+        end: "bottom center",
+    }
 })
+
+gsap.from(".article__bar", {
+    height: 487.5,
+    duration: 2,
+    scrollTrigger: {
+        trigger: ".article__bar",
+        start: "top center",
+        end: "bottom center",
+    }
+})
+
 
 
 gsap.from(".gspa__title", {
@@ -42,9 +74,7 @@ gsap.from(".presentation__card", {
     delay: 1,
     stagger: {
         each: 0.2,
-        from: "center",
-        grid: [1,0,2],
-        amount: 1,
+        amount: 0.3,
     },
     scrollTrigger: {
         trigger: ".gspa__title",
@@ -53,5 +83,36 @@ gsap.from(".presentation__card", {
     }
 })
 
+gsap.from(".convaincu", {
+    height: 0,
+    duration: 1,
+    scrollTrigger: {
+        trigger : ".convaincu",
+        start: "top center",
+        end: "bottom center",
+    }
+})
+
+
+gsap.from(".convaincu__title", {
+    x: -300,
+    duration: 1.5,
+    delay: 0.5,
+})
+
+gsap.from(".gsap__contact", {
+    y: 100,
+    opacity: 0,
+    duration: 0.5,
+    stagger: {
+      each: 0.2,
+      amount: 0.3,
+    },
+    scrollTrigger: {
+        trigger: ".contact__title",
+        start: "top center",
+        end: "bottom center",
+    }
+})
 
 
